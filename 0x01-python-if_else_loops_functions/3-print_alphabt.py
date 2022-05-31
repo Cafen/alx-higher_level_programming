f9d880bb025a1f8bc5fb0062e05fc0d079bf2378
Elizabeth Salako <waleade602@gmail.com>
2022-05-31 12:05:20 +0000

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-
-"""Print the alphabet in lowercase, not followed by a new line."""
-for characters in range(97, 123):
-    if chr(characters) != 'q' and chr(characters) != 'e':
-        print(f"{chr(characters)}", end="")
+for c in range(ord('a'), ord('z') + 1):
+    if c != ord('e') and c != ord('q'):
+        print("{:c}".format(c), end="")
