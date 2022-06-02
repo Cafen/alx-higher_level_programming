d273124c4783dd339d9a893fcfb6a2d20de362cf
Elizabeth Salako <waleade602@gmail.com>
2022-06-02 14:12:44 -0500

    task fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    arg_len = len(argv) - 1
-    if arg_len == 1:
+    #arg_len = len(argv) - 1
+    if len(argv) == 1:
         print("0 arguments.")
-    elif arg_len == 2:
-        print("{} argument:".format(arg_len))
-        print("{}: {}".format(arg_len, argv[1]))
-    elif arg_len > 2:
-        print("{} arguments:".format(arg_len))
-        for i in range(1, arg_len + 1):
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("{}: {}".format(len(argv) - 1, argv[1]))
+    elif len(argv) > 2:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
